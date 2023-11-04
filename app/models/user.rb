8e55cad964d8fff329e20e62ac412a24a50f361e
rega1237 <rega1237@gmail.com>
2023-11-04 08:31:51 -0400

    add validation to user model and create the reference with stripe

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,26 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
+  before_validation :create_stripe_reference, on: :create
+
   extend Devise::Models
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  validates :stripe_id, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :name, presence: true
+
+  def create_stripe_reference
+    customer = Stripe::Customer.create(email: email)
+    self.stripe_id = customer.id
+  end
+
+  def retrieve_stripe_reference
+    Stripe::Customer.retrieve(stripe_id)
+  end
 end
