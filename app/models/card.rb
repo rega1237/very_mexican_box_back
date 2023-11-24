e70a6e4d75e6e6fce019aa1df0944163560925f7
rega1237 <rega1237@gmail.com>
2023-11-24 08:54:23 -0400

    card model

---
+++ app/models/card.rb
@@ -0,0 +1,80 @@
+class Card < ApplicationRecord
+  attr_accessor :card_token
+
+  belongs_to :user
+
+  validates :stripe_id, presence: true, uniqueness: true
+  validates :last_four, presence: true, uniqueness: true 
+
+  before_validation :create_stripe_card_reference, on: :create
+
+  def create_stripe_card_reference
+    oldcard = user.cards.where(default: true)
+
+    if from_subscription
+        puts "from subscription"
+        puts "---------------------------------"
+        response = user.create_new_source(card_token)
+  
+        set_values(response, oldcard)
+        
+        update_default_source(response.id)
+        update_default_db if default
+    elsif !card_exist?
+      response = user.create_new_source(card_token)
+
+      set_values(response, oldcard)
+
+      if default
+        update_default_db
+        update_default_source(response.id)
+      elsif !oldcard.empty? && !default
+        update_default_source(oldcard[0].stripe_id)
+      end
+    end
+  end
+
+  private
+
+  def set_values(response, oldcard)
+    self.stripe_id = response.id
+    self.last_four = response.last4
+    self.brand = response.brand
+    self.exp_month = response.exp_month
+    self.exp_year = response.exp_year
+    self.name_on_card = response.name
+    if oldcard.empty?
+      self.default = true
+    else
+      self.default = default
+    end
+  end
+
+  def card_exist?
+    return false if user.cards.empty?
+
+    all_cards = user.cards
+
+    card_token_last_four = Stripe::Token.retrieve(card_token).card.last4
+
+    all_cards.filter { |card| card.last_four == card_token_last_four }.empty? ? false : true
+    puts "card exist"
+  end
+
+  def update_default_source(card_id)
+    Stripe::Customer.update(
+      user.stripe_id,
+      { default_source: card_id }
+    )
+  end
+
+  def update_default_db
+    puts "update default db"
+    puts "---------------------------------"
+    all_cards = user.cards
+
+    all_cards.update_all(default: false)
+
+    self.default = true
+  end
+end
