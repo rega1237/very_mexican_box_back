4d4ba3c3f63574ce28ad8e8f63b4e38dc8a27181
rega1237 <rega1237@gmail.com>
2023-11-24 08:56:23 -0400

    add routes for card

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   resources :subscriptions, only: %i[show create update]
+  resources :cards, only: %i[index show create destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
