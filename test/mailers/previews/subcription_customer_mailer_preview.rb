4be905e65729db4056eb6b830bd9b888da1d28be
rega1237 <rega1237@gmail.com>
2023-11-28 09:57:42 -0400

    rubocop

--- test/mailers/previews/subcription_customer_mailer_preview.rb
+++ test/mailers/previews/subcription_customer_mailer_preview.rb
@@ -3,6 +3,6 @@ class SubcriptionCustomerMailerPreview < ActionMailer::Preview
   def new_subscription_to_customer_test
     subscription = Subscription.new(user_id: 2, plan_id: 1, active: true, card_token: 'tok_visa', change_default: false)
     subscription.save
-    SubcriptionCustomerMailer.with(subscription: subscription).new_subscription_to_customer
+    SubcriptionCustomerMailer.with(subscription:).new_subscription_to_customer
   end
 end
