777cdb15cc3ca35a1b5188d27465ba08890885a1
rega1237 <rega1237@gmail.com>
2023-12-26 16:25:58 -0400

    create the router to get user subscriptions and cancel subscription

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   resources :subscriptions, only: %i[show create update]
   resources :cards, only: %i[index show create destroy update]
 
+  get 'users/:id/subscriptions', to: 'users#user_subscriptions'
+  
   get 'users/:id', to: 'users#show'
+
+  put 'subscriptions/:id/cancel_stripe_subscription', to: 'subscriptions#cancel_stripe_subscription'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
